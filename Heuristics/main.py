cf5cdd85d267126980fa8df79cafa04c6ff15f76
Miguel Hernandez-Cabrera <jose.miguel.heca@gmail.com>
2022-01-06 12:52:16 +0100

    Add to main checkers with Prim and parsing file name

--- Heuristics/main.py
+++ Heuristics/main.py
@@ -10,10 +10,11 @@ import time
 start_time = time.time()
 
 parser = ArgumentParser(description='OTDM Lab Heuristics')
-parser.add_argument('-c', '--configFile', nargs='?', type=Path,
-					default=Path(__file__).parent / 'blob.dat', help='specifies the config file')
+parser.add_argument('-d', '--data', nargs='?', default='blob.dat', help='specifies the data file')
 args = parser.parse_args()
-config = DATParser.parse(args.configFile)
+# config = DATParser.parse(args.configFile)
+config = DATParser.parse(Path(args.data))
+
 
 A = []
 for row in config.A:
@@ -46,7 +47,7 @@ MST.add_nodes_from(nodes)
 for node1, node2, weight in edges:
 	MST.add_edge(node1, node2, weight=weight)
 nx.draw_networkx(MST, node_size=50, font_size=4)
-plt.savefig('Graph_MST_bloob.png', dpi=150)
+plt.savefig('Graph_MST_blob.png', dpi=150)
 plt.close()
 
 # Remove the k-1 edges with largest weights in the MST
@@ -62,10 +63,12 @@ print("--- %s seconds ---" % (time.time() - start_time))
 
 total_cost = 0
 # Printing the result and the graphs
+sub_graphs_nodes = {}
 for i, sg in enumerate(sub_graphs):
 	print("subgraph {} has {} nodes".format(i, sg.number_of_nodes()))
 	print("\tNodes:", sg.nodes(data=False))
 	print("\tEdges:", sg.edges())
+	sub_graphs_nodes[i] = list(sg.nodes(data=False))
 	pos = nx.spring_layout(sg)
 	c = []
 	c.append(center(sg)[0])
@@ -75,35 +78,41 @@ for i, sg in enumerate(sub_graphs):
 	nx.draw_networkx_edges(sg, pos)
 	nx.draw_networkx_nodes(sg, pos, nodelist=c, node_color='r', node_size=50)
 	nx.draw_networkx_labels(sg, pos, font_size=4)
-	plt.savefig('Subgraph_'+str(i)+'_bloob.png', dpi=150)
+	plt.savefig('Subgraph_'+str(i)+'_blob.png', dpi=150)
 	plt.close()
 
 
 print('Total cost of the solution is: ', total_cost)
-print('-----Checking using built in functions of networkx------')
 
-G2 = nx.Graph()
-i = 1
-for row in D:
-	j=1
-	for item in row:
-		G2.add_edge(i, j, weight=item)
-		j+=1
-	i+=1
+print('\n-----Checking using built-in functions of Networkx------')
 
-T=nx.minimum_spanning_tree(G2)
+G_check = nx.from_numpy_array(np.array(D))
+MST_check = nx.algorithms.tree.mst.minimum_spanning_tree(G_check, algorithm='prim')
+edges_check = list(MST_check.edges.data("weight"))
 
-edges2 = list(T.edges(data=True))
+# Remove the k-1 edges with largest weights in the MST
 for i in range(config.k-1):
-	edge_remove = max(edges2, key=lambda t: t[2]['weight'])
-	edges2.remove(edge_remove)
-	T.remove_edge(edge_remove[0], edge_remove[1])
+	edge_remove = max(edges_check, key=lambda t: t[2])
+	edges_check.remove(edge_remove)
+	MST_check.remove_edge(edge_remove[0], edge_remove[1])
 
 # Extracting subgraphs from the MST - k-1 edges with largest weights
-sub_graphs = (T.subgraph(c) for c in nx.connected_components(T))
+sub_graphs_check = (MST_check.subgraph(c) for c in nx.connected_components(MST_check))
 
 # Printing the result and the graphs
-for i, sg in enumerate(sub_graphs):
+sg_nodes = {}
+for i, sg in enumerate(sub_graphs_check):
+	sg_nodes[i] = list(sg.nodes(data=False))
 	print("subgraph {} has {} nodes".format(i, sg.number_of_nodes()))
 	print("\tNodes:", sg.nodes(data=False))
 	print("\tEdges:", sg.edges())
+
+# Printing check result
+checker = 0
+for k, v in sg_nodes.items():
+	checker += bool(set(v).difference(sub_graphs_nodes[k]))
+if checker == 0:
+	print('\n-----MST subpgraphs checks passed-----')
+else:
+	print('\n-----WARNING: MST subgraphs checks failed!-----')
+
